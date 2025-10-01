09857c9a4818766fdabc92a0315989c8537bab95
Andrew Ford <andrew@andrewford.co.nz>
2025-10-01 16:50:28 +1300

    Remove frontend React app and related config files

--- frontend/app.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from '@tanstack/react-start/config';
-
-export default defineConfig({
-  routesDirectory: './app/routes',
-  routeTreeGeneration: './app/routeTree.gen.ts',
-});
